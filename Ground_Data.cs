175b037358f0e5d8d6912e62396baf235f629d81
Wareureung <49370668+Wareureung@users.noreply.github.com>
2021-06-14 05:01:29 +0900

    [ 2021.06.14.0459 ] - 원블럭 표시적 위치 저장하기용

---
+++ Ground_Data.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground_Data : MonoBehaviour
+{
+    public struct Map_Num
+    {
+        public int nx;
+        public int ny;
+    }
+    public Map_Num block_num;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void Set_Block_Number(int x, int y)
+    {
+        block_num.nx = x;
+        block_num.ny = y;
+    }
+}
